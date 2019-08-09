91d84d6b5d0b909d8442f192c625fce354397dcb
Joe Cygnarowicz <github@cygnarowicz.com>
2019-08-08 22:26:30 -0700

    add bootstrap and prop-types dependencies

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 ReactDOM.render(
