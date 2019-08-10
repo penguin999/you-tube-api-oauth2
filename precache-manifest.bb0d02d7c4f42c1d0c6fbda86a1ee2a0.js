b26fc95326d3ddb374551a4c3bf051d0b76dedf7
Joe Cygnarowicz <github@cygnarowicz.com>
2019-08-10 16:38:01 -0700

    Updates

---
+++ precache-manifest.bb0d02d7c4f42c1d0c6fbda86a1ee2a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3782d0ee08528c130c1e6e91a0070b25",
+    "url": "/you-tube-api-oauth2/index.html"
+  },
+  {
+    "revision": "70ea93996b2604e4b8af",
+    "url": "/you-tube-api-oauth2/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4789e89b6dc2c2f2ab48",
+    "url": "/you-tube-api-oauth2/static/css/main.96c5c70f.chunk.css"
+  },
+  {
+    "revision": "70ea93996b2604e4b8af",
+    "url": "/you-tube-api-oauth2/static/js/2.5e44dba0.chunk.js"
+  },
+  {
+    "revision": "4789e89b6dc2c2f2ab48",
+    "url": "/you-tube-api-oauth2/static/js/main.2fed34d8.chunk.js"
+  },
+  {
+    "revision": "bf507b14f27302cdc7f5",
+    "url": "/you-tube-api-oauth2/static/js/runtime~main.090a120c.js"
+  },
+  {
+    "revision": "f23cbab7eaa6f6143603f962bb7c6a32",
+    "url": "/you-tube-api-oauth2/static/media/magnifying-glass.f23cbab7.svg"
+  },
+  {
+    "revision": "7a781beb7bf584bbe9554e6dbb6f5d56",
+    "url": "/you-tube-api-oauth2/static/media/yt-logo-mono-light.7a781beb.png"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/you-tube-api-oauth2/static/media/yt-logo-rgb-light.07b8560a.png"
+  }
+]);
